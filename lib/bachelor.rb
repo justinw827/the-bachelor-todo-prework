3433f537bfa6625f7640bcc94b248141d106168e
Justin Wang <justinw827@aol.com>
2018-07-26 04:31:46 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,7 +23,14 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, personArray|
+    personArray.each do |person|
+      if person["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
