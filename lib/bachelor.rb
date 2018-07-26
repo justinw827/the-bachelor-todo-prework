abdff306b349c6b4e8bfd931ecef7e6251b794a6
Justin Wang <justinw827@aol.com>
2018-07-26 04:24:31 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data, season)
+  winner = " "
   data[season].each do |person|
     if person["status"] == "Winner"
       nameArray = person["name"].split(' ')
