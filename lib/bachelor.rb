30a83d2635b454a00a3b3cb30f18b7757ab5128d
Justin Wang <justinw827@aol.com>
2018-07-26 04:45:07 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = " "
+  data[season].each do |person|
+    if person["status"] == "Winner"
+      nameArray = person["name"].split(' ')
+      winner = nameArray[0]
+    end
+  end
+
+  return winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, personArray|
+    personArray.each do |person|
+      if person["occupation"] == occupation
+        return person["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, personArray|
+    personArray.each do |person|
+      if person["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, personArray|
+    personArray.each do |person|
+      if person["hometown"] == hometown
+        return person["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  average = 0
+  data[season].each do |person|
+    sum += person["age"].to_f
+  end
+
+  average = sum / data[season].length
+
+  return average.round
 end
